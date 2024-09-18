ddca7ee7159de15205323ab3a35233478bb25e14
praveenk3 <praveenk@revidd.com>
2024-09-18 12:25:49 +0530

    initial setup

---
+++ covid19/src/app/signIn/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PrimaryButton from "../../components/primaryButton"
+
+const SignIn=()=>{
+    return(<div className="flex flex-col w-full h-full justify-center items-center">
+        praveen SignIn
+        <PrimaryButton/>
+    </div>)
+}
+
+export default SignIn
---
+++ covid19/src/components/primaryButton/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';    
+import Button from '@mui/material/Button';  
+
+const PrimaryButton=()=> {  
+  return (  
+      <Button variant="contained">Contained</Button>   
+  );  
+}  
+
+export default PrimaryButton;
